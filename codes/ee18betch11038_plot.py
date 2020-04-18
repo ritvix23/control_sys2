3246890272565603c02a5922938cf8d5bfd59186
Ritwik Sahani <ee18btech11038@iith.ac.in>
2020-04-18 23:56:58 +0530

    Add files via upload

---
+++ codes/ee18betch11038_plot.py
@@ -0,0 +1,20 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+K = np.pi/3
+w= np.linspace(pow(10,-1), pow(10,1),10000)
+
+mag = 20*np.log(K/w)
+phase = -90 - w*180/np.pi
+
+
+plt.semilogx(w,mag)
+plt.semilogx(w, phase)
+plt.semilogx(w,-150*np.ones(10000), color ='purple' , linestyle = 'dashed')
+plt.semilogx(w,np.zeros(10000), color ='black' , linestyle = 'dashed')
+plt.semilogx((np.pi/3)*np.ones(10000), np.linspace(-200,50, 10000), color = 'red', linestyle ='dashed' )
+plt.ylim([-200, 50])
+plt.ylabel('phase in degrees & magnitude in dB')
+plt.xlabel('frequency in radian')
+plt.legend(['Magnitude', 'Phase','-150 degree line', 'dB 0 line', 'pi/3 radian line'], loc='upper right')
+plt.show()
